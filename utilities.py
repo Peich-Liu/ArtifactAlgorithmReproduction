e5c92e006e34ba133aa2af70f99b3d38e58256a6
Pc <80606843+Peich-Liu@users.noreply.github.com>
2025-02-23 21:08:30 +0100

    add dataloader

---
+++ utilities.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import pyedflib
+import datetime
+
+def readEdfFile(fileName):
+    ''' 
+    Reads .edf file, returns signal, start time, channel names and sampling frequency
+    - this need to be modified to last raw is not EEG
+    - this function is assumed that every signal has same length
+    '''
+
+    f = pyedflib.EdfReader(fileName)
+    n = f.signals_in_file
+    channelNames = f.getSignalLabels()
+    samplFreq =f.getSampleFrequency(0)
+    #read start time
+    fileStartTime=datetime.datetime(f.startdate_year, f.startdate_month, f.startdate_day, f.starttime_hour, f.starttime_minute, f.starttime_second, f.starttime_subsecond)
+    
+    data = np.zeros((f.getNSamples()[0], n))
+    for i in np.arange(n-3):
+        data[:, i] = f.readSignal(i)
+    dataDF=pd.DataFrame(data, columns=channelNames)
+
+    return dataDF,fileStartTime, channelNames, samplFreq 
